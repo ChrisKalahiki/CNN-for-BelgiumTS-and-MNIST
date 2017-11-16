66577c3529c815c621bf6c95ff451de1310da308
Sieve <sieve32@gmail.com>
2017-11-15 18:37:41 -0600

    Main touchup
    Creates 2 functions: train_network() and test_network() that call to do their respective tasks, so they are no longer hard-coded in main()

--- CNN/cnn_RMR.py
+++ CNN/cnn_RMR.py
@@ -144,8 +144,47 @@ def resizeImages(images):
                 for image in images]
     return reimages
 
-# This section will change to the new data set
+def train_network(mnist_classifier, logging_hook):
+    train_data, train_labels = getData('BelgiumTS/Training')
+
+    train_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={"x": train_data},
+        y=train_labels,
+        batch_size=100,
+        num_epochs=None,
+        shuffle=True)
+    mnist_classifier.train(
+        input_fn=train_input_fn,
+        steps=100,  # 20000
+        hooks=[logging_hook])
+
+def test_network(mnist_classifier):
+    eval_data, eval_labels = getData('BelgiumTS/Testing')
+
+    eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={"x": eval_data},
+        y=eval_labels,
+        num_epochs=1,
+        shuffle=False)
+    eval_results = mnist_classifier.evaluate(input_fn=eval_input_fn)
+    print(eval_results)
+
 def main(unused_argv):
+    # Create the Estimator
+    mnist_classifier = tf.estimator.Estimator(
+        model_fn=cnn_model_fn, model_dir="/tmp/mnist_convnet_model")
+
+    # Set up logging for predictions
+    # Log the values in the "Softmax" tensor with label "probabilities"
+    tensors_to_log = {"probabilities": "softmax_tensor"}
+    logging_hook = tf.train.LoggingTensorHook(
+        tensors=tensors_to_log, every_n_iter=50)
+
+    train_network(mnist_classifier, logging_hook)
+    test_network(mnist_classifier)
+
+# This section will change to the new data set
+def oldmain(unused_argv):
     # Load training and eval data
     #mnist = tf.contrib.learn.datasets.load_dataset("mnist")
     #train_data = mnist.train.images  # Returns np.array
@@ -175,7 +214,7 @@ def main(unused_argv):
         shuffle=True)
     mnist_classifier.train(
         input_fn=train_input_fn,
-        steps=20000,
+        steps=100,#20000
         hooks=[logging_hook])
 
     # Evaluate the model and print results
